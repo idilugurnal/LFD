6fcd30420e120ec5a4740818aa25c617151eea47
idil <idilugurnal@hotmail.com>
2019-05-19 18:35:09 +0300

    %55 with SVC

--- code.py
+++ code.py
@@ -8,18 +8,30 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 import csv
-from keras import Sequential
-from keras.layers import Dense
+from sklearn.feature_selection import SelectFromModel
 
 def randomForest(X_train , y_train, X_test):
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
     X_test = sc.fit_transform(X_test)
 
+    sel = SelectFromModel(RandomForestClassifier(n_estimators=5))
+    sel.fit(X_train, y_train)
+
+
+    selected_features = X_train[: , (sel.get_support())]
+
+    print(selected_features.shape[1])
+
+
 
     classifier = RandomForestClassifier(n_estimators = 5)
-    classifier.fit(X_train , y_train)
-    y_pred  = classifier.predict(X_test)
+    classifier.fit(selected_features , y_train)
+
+    selected_features = X_test[: , (sel.get_support())]
+    print(selected_features.shape[1])
+
+    y_pred = classifier.predict(selected_features)
 
     writeBack(y_pred)
     print(y_pred)
@@ -62,60 +74,6 @@ def writeBack(y_pred):
             writer = csv.writer(writeFile)
             writer.writerow(row)
 
-
-def NeuralNetwork(X_train , y_train , X_test):
-    sc = StandardScaler()
-    X_train = sc.fit_transform(X_train)
-    X_test = sc.fit_transform(X_test)
-
-    pca = PCA(n_components=20)
-    X_scaled = pca.fit_transform(X_train)
-
-    X_test_scaled = pca.fit_transform(X_test)
-
-    #First Layer
-
-    classifier = Sequential()
-    classifier.add(Dense(10 , activation = 'relu' , kernel_initializer='random_normal' , input_dim = 20))
-
-    #Second Layer
-
-    classifier.add(Dense(10, activation='relu', kernel_initializer='random_normal'))
-
-
-    #Output Layer
-
-    classifier.add(Dense(1, activation='sigmoid', kernel_initializer='random_normal'))
-
-    # Compiling the neural network
-    classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-    # Fitting the data to the training dataset
-    classifier.fit(X_scaled, y_train, batch_size=5, epochs=400)
-
-    y_pred = classifier.predict(X_test_scaled)
-
-    '''for i in range(0 , len(y_pred)):
-        if(y_pred[i] > 0.5):
-            y_pred[i] = 1
-        else:
-            y_pred[i] = 0
-
-
-    print(y_pred)
-
-    result = 0
-
-    #print(len(y_pred))
-    #print(y_test)
-
-
-    for i in range(0 , len(y_pred)):
-        if(int(y_pred[i]) == y_test.iloc[i]):
-            result+=1
-
-    print(result / len(y_pred))'''
-    writeBack(y_pred)
 
 
 
--- code2.py
+++ code2.py
@@ -1,1 +1,66 @@
+from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+import sklearn.tree as tree
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+import csv
+from sklearn.feature_selection import SelectFromModel
+from sklearn.feature_selection import SelectKBest, chi2, f_regression
 
+
+
+
+def writeBack(y_pred):
+    for i in range(0, len(y_pred) + 1):
+        if i == 0:
+            with open('output.csv', 'w') as writeFile:
+                writer = csv.writer(writeFile)
+                writer.writerow(["ID", "Predicted"])
+            continue
+        row = [i, int(y_pred[i - 1])]
+        with open('output.csv', 'a') as writeFile:
+            writer = csv.writer(writeFile)
+            writer.writerow(row)
+
+
+
+def classify(X_train , y_train , X_test):
+
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train, y_train)
+    X_test = sc.fit_transform(X_test)
+
+
+    svc = SVC(kernel = 'poly' , gamma = 1 , C = 1, degree = 4).fit(X_train , y_train)
+    y_pred = svc.predict(X_test)
+
+    count = 0
+
+    for i in y_pred:
+        if i == 1:
+            count+=1
+    print(count)
+    print(y_pred)
+
+    writeBack(y_pred)
+
+
+
+
+if __name__ == '__main__':
+    train_data = pd.read_csv('train.csv')
+    test_data = pd.read_csv('test.csv')
+
+    X_train = train_data.iloc[:, 0: -1]
+    y_train = train_data.iloc[:, -1]
+    X_test = test_data.iloc[:, 0:]
+
+    # X_train , X_test , y_train , y_test = train_test_split(X , y , test_size=0.2 , random_state=1)
+
+    classify(X_train, y_train, X_test)
